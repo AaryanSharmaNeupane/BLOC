4ab485006ae5a3b03423ee02bae174f4355aca73
Monika Gautam <monikagm11@gmail.com>
2023-06-03 20:21:23 +0545

    consumer concept

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,50 +9,48 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocListener<CounterCubit, CounterState>(
-        listener: (context, state) {
-          if (state.wasIncremented == true) {
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-              content: Text("Increment"),
-              duration: Duration(microseconds: 300),
-            ));
-          } else {
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-              content: Text("Decrement"),
-              duration: Duration(microseconds: 300),
-            ));
-          }
-        },
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BlocBuilder<CounterCubit, CounterState>(
-              builder: (context, state) {
-                return Text(
-                  state.counterValue.toString(),
-                  style: const TextStyle(fontSize: 30),
-                );
-              },
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                FloatingActionButton(
-                  onPressed: () =>
-                      BlocProvider.of<CounterCubit>(context).decrement(),
-                  tooltip: "Decrement",
-                  child: const Icon(Icons.remove),
-                ),
-                FloatingActionButton(
-                  onPressed: () =>
-                      BlocProvider.of<CounterCubit>(context).increment(),
-                  tooltip: "Increment",
-                  child: const Icon(Icons.add),
-                )
-              ],
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BlocConsumer<CounterCubit, CounterState>(
+            listener: (context, state) {
+              if (state.wasIncremented == true) {
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text("Increment"),
+                  duration: Duration(microseconds: 300),
+                ));
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text("Decrement"),
+                  duration: Duration(microseconds: 300),
+                ));
+              }
+            },
+            builder: (context, state) {
+              return Text(
+                state.counterValue.toString(),
+                style: const TextStyle(fontSize: 30),
+              );
+            },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              FloatingActionButton(
+                onPressed: () =>
+                    BlocProvider.of<CounterCubit>(context).decrement(),
+                tooltip: "Decrement",
+                child: const Icon(Icons.remove),
+              ),
+              FloatingActionButton(
+                onPressed: () =>
+                    BlocProvider.of<CounterCubit>(context).increment(),
+                tooltip: "Increment",
+                child: const Icon(Icons.add),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
